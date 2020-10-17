c49d0151bfdac474c1ced8e000ae4c6c963f05a7
Osmar Rodrigues <mazinho_skt42@hotmail.com>
2020-10-17 17:14:02 -0300

    Criação das Entidades

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,36 @@
+﻿using QuickBuy.Dominio.ObjetoDeValor;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UsuarioId { get; set; }
+        public DateTime DataPrevisaoEntrega { get; set; }
+        public String CEP { get; set; }
+        public String Estado { get; set; }
+        public String Cidade { get; set; }
+        public String EnderecoCompleto { get; set; }
+        public int NumeroEndereco { get; set; }
+
+
+
+        public int FormaPagamentoId { get; set; }
+
+        public FormaPagamento FormaPagamento { get; set; }
+
+
+
+
+        //Pedido deve ter pelo menos um Item de pedido ou muitos Itens pedidos
+        public ICollection<ItemPedido> ItensPedido { get; set; }
+
+
+
+    }
+}
---
+++ QuickBuy.Dominio/Entidades/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public String Descricao { get; set; }
+        public decimal Preco { get; set; }
+
+
+
+    }
+}
