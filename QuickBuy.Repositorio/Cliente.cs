77fe1667ded73531017cfba734a2406c35f648d6
Osmar Rodrigues <mazinho_skt42@hotmail.com>
2020-10-20 14:05:53 -0300

    Criação Class no Proj Repositorio, implementação dos contratos.

---
+++ QuickBuy.Repositorio/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using QuickBuy.Dominio.Entidades;
+using QuickBuy.Repositorio.Repositorios;
+using System;
+
+namespace QuickBuy.Repositorio
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+            var usuarioRepositorio = new UsuarioRepositorio();
+            var usuario = new Usuario();
+
+            usuarioRepositorio.Adicionar(usuario);
+
+        }
+    }
+}
