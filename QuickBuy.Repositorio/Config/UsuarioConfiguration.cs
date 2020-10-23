fae41f3c6f75338be876218312e9089405d4b09f
Osmar Rodrigues <mazinho_skt42@hotmail.com>
2020-10-23 01:01:40 -0300

    Criando classes de mapeamento e o metodo de configurção no contexto.

---
+++ QuickBuy.Repositorio/Config/UsuarioConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using QuickBuy.Dominio.Entidades;
+
+namespace QuickBuy.Repositorio.Config
+{
+    public class UsuarioConfiguration : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+
+}
