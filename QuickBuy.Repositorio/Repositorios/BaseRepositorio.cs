1aaad097c630a6996c32df657125e5e9867df435
Osmar Rodrigues <mazinho_skt42@hotmail.com>
2020-10-26 00:03:27 -0300

    Conexão das classes repositori com o banco.

--- QuickBuy.Repositorio/Repositorios/BaseRepositorio.cs
+++ QuickBuy.Repositorio/Repositorios/BaseRepositorio.cs
@@ -1,20 +1,28 @@
 ﻿using QuickBuy.Dominio.Contratos;
+using QuickBuy.Repositorio.Contexto;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace QuickBuy.Repositorio.Repositorios
 {
     public class BaseRepositorio<TEntity> : IBaseRepositorio<TEntity> where TEntity : class
     {
-        public BaseRepositorio()
+
+
+        // Pegando a instancia (DbContext) criada pela propria infra do asp net core e armezanando internamente na classe BaseRepositorio.
+        // Conexão das classes com banco de dados, pegando as configurações de DbContext, q são configurads/referenciadas na classe Startup.
+        
+        public readonly QuickBuyContexto _quickBuyContexto;
+        public BaseRepositorio(QuickBuyContexto quickBuyContexto)
         {
-         
+            _quickBuyContexto = quickBuyContexto;
         }
 
         public void Adicionar(TEntity entity)
         {
-            throw new NotImplementedException();
+            _quickBuyContexto.Set<TEntity>().Add(entity);
         }
 
         public void Atualizar(TEntity entity)
@@ -29,7 +37,7 @@ namespace QuickBuy.Repositorio.Repositorios
 
         public IEnumerable<TEntity> ObterTodos()
         {
-            throw new NotImplementedException();
+            return _quickBuyContexto.Set<TEntity>().ToList();
         }
 
         public void Remover(TEntity entity)
