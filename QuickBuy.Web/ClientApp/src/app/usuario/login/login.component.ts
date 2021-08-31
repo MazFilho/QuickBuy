2c9dde987d27f32da4f67b8852febb2bbb05c250
Osmar <mazinho_skt42@hotmail.com>
2021-08-31 20:00:24 -0300

    returnUrl

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,24 +1,35 @@
-import { Component } from "@angular/core"
+import { Component, OnInit } from "@angular/core"
 import { Usuario } from "../../modelo/usuario";
-import { Router } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
   public usuario: Usuario;
 
-  constructor(private router: Router) {
-   this.usuario = new Usuario();
+  public returnUrl: string;
+
+  constructor(private router: Router,
+              private activatedRouter: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.usuario = new Usuario();
+
+    this.returnUrl = this.activatedRouter.snapshot.queryParams['returnUrl'];
+    if (!this.returnUrl) {
+      this.returnUrl = '/';
+    }
   }
 
   public entrar(): void {
     if (this.usuario.email == "mike@easy.com" && this.usuario.senha == "1505") {
       sessionStorage.setItem("usuario-autenticado", "1");
-      this.router.navigate(['/']);
+      this.router.navigate([this.returnUrl]);
     }
   }
 
