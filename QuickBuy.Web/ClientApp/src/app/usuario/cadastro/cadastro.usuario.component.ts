a296cecd8dd4626b05970d093fd31c8075a340be
Osmar <mazinho_skt42@hotmail.com>
2021-09-27 15:54:01 -0300

    Config cadatrarUser

--- QuickBuy.Web/ClientApp/src/app/usuario/cadastro/cadastro.usuario.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/cadastro/cadastro.usuario.component.ts
@@ -1,10 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { UsuarioServico } from "../../servicos/usuario/usuario.servico";
+import { Usuario } from "../../modelo/usuario";
 
 @Component({
     selector: "cadastro-usuario",
     templateUrl: "./cadastro.usuario.component.html",
     styleUrls: ["./cadastro.usuario.component.css"]
 })
-export class CadastroUsuarioComponent {
+export class CadastroUsuarioComponent implements OnInit {
 
+    public usuario: Usuario;
+
+    constructor(private usuarioServico: UsuarioServico) { }
+
+    ngOnInit() {
+        this.usuario = new Usuario();
+    }
+
+    public cadastrar() {
+        this.usuarioServico.cadastrarUsuario(this.usuario)
+            .subscribe(
+                usuarioJson => { },
+                e => { }
+            );
+    }
 }
